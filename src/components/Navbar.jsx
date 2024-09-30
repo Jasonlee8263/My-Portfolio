bad6fa01e7ce65ec8b51f298fa821a964afd406a
jason <whdgur826333@gmail.com>
2024-09-30 15:56:11 -0600

    finished navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,16 +7,18 @@ const Navbar = () => {
   const [active, setactive] = useState("")
   return (
     <nav className="sm:px-16 px-6 w-full flex items-center py-5 fixed top-0 z-20">
-      <Link 
-      to="/"
-      className="flex items-center gap-2"
-      onClick={()=>{
-        setactive("");
-        window.scrollTo(0,0);
-      }}>
-        <img src={logo} alt="logo" className="w-24 h-24 object-contain"/>
-      </Link>
-      <ul className="flex flex-row">
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link 
+        to="/"
+        className="flex items-center gap-2"
+        onClick={()=>{
+          setactive("");
+          window.scrollTo(0,0);
+        }}>
+          <img src={logo} alt="logo" className="w-24 h-24 object-contain"/>
+        </Link>
+      </div>
+      <ul className="list-none hidden sm:flex flex-row gap-10">
         {navLinks.map((link) => (
           <li
           key={link.id}
